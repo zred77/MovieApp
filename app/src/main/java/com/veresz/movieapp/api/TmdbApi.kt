51c159f4ae3d193dfbbd6436e48fbbfdea3d1e90
zred <zred77@gmail.com>
2018-09-30 12:56:50 +0200

    add tmdb api

---
+++ app/src/main/java/com/veresz/movieapp/api/TmdbApi.kt
@@ -0,0 +1,45 @@
+package com.veresz.movieapp.api
+
+import com.veresz.movieapp.BuildConfig
+import com.veresz.movieapp.model.Movie
+import com.veresz.movieapp.model.MovieList
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level.BODY
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface TmdbApi {
+
+    @GET("3/movie/now_playing")
+    fun nowPlaying(@Query("language") language: String? = null,
+                   @Query("page") page: Int = 1): Call<MovieList>
+
+    @GET("3/movie/{movieId}")
+    fun movieDetail(@Path("movieId") movieId: Int): Call<Movie>
+
+    @GET("3/search/movie")
+    fun searchMovie(@Query("query") query: String? = null,
+                    @Query("language") language: String? = null,
+                    @Query("page") page: Int = 1): Call<Movie>
+
+    companion object {
+
+        fun create(): TmdbApi {
+            val client = OkHttpClient.Builder()
+                    .addInterceptor(TmdbInterceptor())
+                    .addInterceptor(HttpLoggingInterceptor().setLevel(BODY))
+                    .build()
+            return Retrofit.Builder()
+                    .baseUrl(BuildConfig.TMDB_BASE_URL)
+                    .client(client)
+                    .addConverterFactory(MoshiConverterFactory.create())
+                    .build()
+                    .create(TmdbApi::class.java)
+        }
+    }
+}
